441128968da4c67fde909aa832a956329c0347d8
Maxime Fondu <maximefondu@icloud.com>
2025-03-03 11:24:53 +0100

    remove font next js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
 import { QueryProvider } from '@digico/utils'
 import { Toaster } from 'sonner'
@@ -14,16 +13,9 @@ type Props = Readonly<{
     children: React.ReactNode
 }>
 
-const inter = Inter({
-    subsets: ['latin'],
-    weight: ['300', '400', '500', '600', '700'],
-    display: 'swap',
-    variable: '--font-inter'
-})
-
 export default function RootLayout({ children }: Props) {
     return (
-        <html lang="fr" className={inter.variable}>
+        <html lang="fr">
             <body>
                 <QueryProvider>{children}</QueryProvider>
                 <Toaster richColors position="bottom-right" />
