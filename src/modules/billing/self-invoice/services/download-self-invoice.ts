56c4f17d9f52b5593eb84fd2075594e56c45803b
Maxime Fondu <maximefondu@icloud.com>
2025-03-18 08:56:34 +0100

    Fix name invoice name pdf

--- src/modules/billing/self-invoice/services/download-self-invoice.ts
+++ src/modules/billing/self-invoice/services/download-self-invoice.ts
@@ -11,12 +11,21 @@ export const downloadSelfInvoice = async (id: number) => {
             Authorization: cookies.get('Authorization')
         }
     })
-        .then((result) => result.blob())
-        .then((result) => {
-            const url = window.URL.createObjectURL(result)
+        .then((response) => {
+            const disposition = response.headers.get('Content-Disposition')
+            let filename = 'document.pdf'
+
+            if (disposition && disposition.includes('filename=')) {
+                filename = disposition.split('filename=')[1].replace(/["']/g, '')
+            }
+
+            return response.blob().then((blob) => ({ blob, filename }))
+        })
+        .then(({ blob, filename }) => {
+            const url = window.URL.createObjectURL(blob)
             const a = document.createElement('a')
             a.href = url
-            a.download = `autofacturation-${id}.pdf`
+            a.download = filename
             document.body.appendChild(a)
             a.click()
             a.remove()
