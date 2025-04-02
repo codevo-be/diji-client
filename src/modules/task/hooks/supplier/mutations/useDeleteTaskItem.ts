922ccca38d97908ce2f01eaf151bca674c2297ce
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-02 16:27:49 +0200

    Fix: update import statements and clean up error handling in useDeleteTaskItem.ts for improved clarity

--- src/modules/task/hooks/supplier/mutations/useDeleteTaskItem.ts
+++ src/modules/task/hooks/supplier/mutations/useDeleteTaskItem.ts
@@ -1,25 +1,21 @@
 'use client'
 
+import { queryClient } from '@digico/utils'
 import { useMutation } from '@tanstack/react-query'
 import { toast } from 'sonner'
 
-import { queryClient } from '@/libs/react-query'
-import {deleteTaskColumn} from "@/modules/task/services/task-column/delete-task-Column";
-import { useErrorStore } from '@/store/errorStore'
-import { HttpServiceErrorProps } from '@/types/httpServiceError'
+import { deleteTaskColumn } from '@tasks/services/task-column/delete-task-Column'
 
 export const useDeleteTaskItem = () => {
-    const { setErrors } = useErrorStore()
     return useMutation({
         mutationFn: deleteTaskColumn,
-        onError: (error: HttpServiceErrorProps) => {
+        onError: (error) => {
             toast.error(error.message)
-            setErrors(error.errors)
         },
         onSuccess: () => {
             queryClient.invalidateQueries({
                 queryKey: ['task-columns']
-            }) //  Met à jour la liste des colonnes & tâches
+            }) // Met à jour la liste des colonnes & tâches
         }
     })
 }
