4ce7e001b50f087f6faa724dbbafc2731949346e
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-02 16:00:02 +0200

    Fix: update import statements in useCreateItem.ts for improved clarity and consistency

--- src/modules/task/hooks/supplier/mutations/useCreateItem.ts
+++ src/modules/task/hooks/supplier/mutations/useCreateItem.ts
@@ -1,20 +1,18 @@
 'use client'
 
+import { queryClient } from '@digico/utils'
 import { useMutation } from '@tanstack/react-query'
 import { toast } from 'sonner'
 
-import { queryClient } from '@/libs/react-query'
-import { createTaskItem } from '@/modules/task/services/task-item/create-task-item'
-import { useErrorStore } from '@/store/errorStore'
-import { HttpServiceErrorProps } from '@/types/httpServiceError'
+import { createTaskItem } from '@tasks/services/task-item/create-task-item'
+
+
 
 export const useCreateItem = () => {
-    const { setErrors } = useErrorStore()
     return useMutation({
         mutationFn: createTaskItem,
-        onError: (error: HttpServiceErrorProps) => {
+        onError: (error) => {
             toast.error(error.message)
-            setErrors(error.errors)
         },
         onSuccess: () => {
             queryClient.invalidateQueries({
