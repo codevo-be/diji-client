353e1a83c922e8de024013d6e22f516eeafd89d7
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-02 14:38:43 +0200

    Fix: refactor TaskItemList component for improved readability and structure by removing LoadingQuery and simplifying the rendering of Table

--- src/modules/task/components/TaskItemList.tsx
+++ src/modules/task/components/TaskItemList.tsx
@@ -1,43 +1,37 @@
-import {useParams} from "next/navigation";
+'use client'
+
+import { useParams } from 'next/navigation'
 
 import { Table, useQueryParams } from '@digico/ui'
 
 import { useReadTaskItem } from '@tasks/hooks/supplier/queries/useReadTaskItem'
 
-import { LoadingQuery } from '@/utils/LoadingQuery'
-
 export const TaskItemList = () => {
     const params = useParams()
-    const task_column_id = params.id
+    const task_column_id = params?.id
 
     const query = useReadTaskItem({
         task_column_id: Number(task_column_id),
         ...useQueryParams()
     })
 
+    const items = query.data?.items ?? []
+
     return (
-        <LoadingQuery query={query}>
-            {({ items }:any) => {
+        <Table items={items}>
+            <Table.Head>ID</Table.Head>
+            <Table.Head>ID de la colonne</Table.Head>
+            <Table.Head>Nom</Table.Head>
+            <Table.Head>Description</Table.Head>
+            <Table.Head>Statut</Table.Head>
+            <Table.Head>Priorité</Table.Head>
 
-                return (
-                    <Table items={items}>
-                        <Table.Head>ID</Table.Head>
-                        <Table.Head>ID de la colonne</Table.Head>
-                        <Table.Head>Nom</Table.Head>
-                        <Table.Head>Description</Table.Head>
-                        <Table.Head>Statut</Table.Head>
-                        <Table.Head>Priorité</Table.Head>
-
-                        <Table.Col name={'id'} />
-                        <Table.Col name={'task_column_id'} />
-                        <Table.Col name={'name'} />
-                        <Table.Col name={'description'} />
-                        <Table.Col name={'status'} />
-                        <Table.Col name={'priority'} />
-                    </Table>
-                )
-            }}
-        </LoadingQuery>
+            <Table.Col name={'id'} />
+            <Table.Col name={'task_column_id'} />
+            <Table.Col name={'name'} />
+            <Table.Col name={'description'} />
+            <Table.Col name={'status'} />
+            <Table.Col name={'priority'} />
+        </Table>
     )
-
 }
