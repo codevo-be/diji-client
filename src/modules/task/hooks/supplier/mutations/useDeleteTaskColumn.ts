5c224bfb8df03d833806a352745d53f7fe35bcc8
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-02 16:01:46 +0200

    Fix: update import statements and clean up code in useDeleteTaskColumn.ts for improved clarity

--- src/modules/task/hooks/supplier/mutations/useDeleteTaskColumn.ts
+++ src/modules/task/hooks/supplier/mutations/useDeleteTaskColumn.ts
@@ -1,25 +1,22 @@
 'use client'
 
+import { queryClient } from '@digico/utils'
 import { useMutation } from '@tanstack/react-query'
 import { toast } from 'sonner'
 
-import { queryClient } from '@/libs/react-query'
-import {deleteTaskColumn} from "@/modules/task/services/task-column/delete-task-Column";
-import { HttpServiceErrorProps } from '@/types/httpServiceError'
+import { deleteTaskColumn } from '@tasks/services/task-column/delete-task-Column'
 
 export const useDeleteTaskColumn = () => {
     return useMutation({
-        // 🔹 Fonction pour supprimer une colonne via API
         mutationFn: deleteTaskColumn,
 
-        onError: (error: HttpServiceErrorProps) => {
+        onError: (error) => {
             toast.error(error.message || "Une erreur s'est produite lors de la suppression.")
         },
 
         onSuccess: () => {
-            toast.success("Colonne supprimée avec succès !")
+            toast.success('Colonne supprimée avec succès !')
 
-            // Rafraîchir la liste des colonnes après suppression
             queryClient.invalidateQueries({ queryKey: ['task-columns'] })
         }
     })
