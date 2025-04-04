868c42b19d80d12d1b5b05fb5464f76b572c1a0e
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-04 13:04:37 +0200

    Feat: update HttpService to use task-projects API and adjust import path

--- src/modules/task/services/read-projects.ts
+++ src/modules/task/services/read-projects.ts
@@ -1,4 +1,4 @@
-import { HttpService } from '@contact/services'
+import { HttpService } from '@task/services'
 import { ProjectType } from '@task/types/project'
 
 export const readProjects = async (params?: Record<string, any>) =>
