17f1d8d7a7e5ee1aa3960b5671cec0eb833359b2
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-04 16:03:01 +0200

    Feat: remove Box component from TaskColumnList for simplified item display

--- src/modules/task/list/components/TaskColumnList.tsx
+++ src/modules/task/list/components/TaskColumnList.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { Box } from '@digico/ui'
-
 type Props = {
     items: {
         id: number
@@ -15,9 +13,7 @@ export const TaskColumnList = ({ items }: Props) => {
     return (
         <div>
             {items.map((col) => (
-                <Box key={col.id} size="default" className="p-4 flex flex-col gap-2" intent="info">
-                    <div className="text-lg font-semibold">{col.name}</div>
-                </Box>
+                <div key={col.id} className="text-lg font-semibold">{col.name}</div>
             ))}
         </div>
     )
