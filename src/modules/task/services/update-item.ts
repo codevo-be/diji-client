127423d7177249f48174f6d7fccb18b4ae9d509c
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-08 09:57:32 +0200

    Feat: update updateItem function to accept TaskItem and use item ID in the request

--- src/modules/task/services/update-item.ts
+++ src/modules/task/services/update-item.ts
@@ -1,8 +1,5 @@
 import { HttpService } from '@task/services'
-import { ContactType } from '@contact/types/contact'
 import { TaskItem } from '@task/types/task_item'
 
-export const updateItem = async (data: ContactType) =>
-    HttpService.put<{
-        data: TaskItem
-    }>(`/`, data)
+export const updateItem = async (data: TaskItem) =>
+    HttpService.put<{ data: TaskItem }>(`/${data.id}`, data)
