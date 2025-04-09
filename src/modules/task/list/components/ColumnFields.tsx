efc541db47f8775cee8030f9f9e03b70828427a8
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-09 16:17:07 +0200

    Feat: simplify ColumnFields component by hiding order field and setting default value

--- src/modules/task/list/components/ColumnFields.tsx
+++ src/modules/task/list/components/ColumnFields.tsx
@@ -4,10 +4,8 @@ export const ColumnFields = () => {
     return (
         <>
             <Form.Group title="Ajouter une liste">
-                <Form.Row>
                     <Form.Field name="name" label={'Nom de la liste'} />
-                    <Form.Field name="order" label={'Ordre'} type={"number"} min={0}/>
-                </Form.Row>
+                    <Form.Field name="order" label={''} type={"number"} defaultValue={999} min={1} hidden={true}/>
             </Form.Group>
         </>
     )
