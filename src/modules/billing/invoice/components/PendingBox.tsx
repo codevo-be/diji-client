8ebd1889ff9da3ee6d77f9c6c6fcba88c70b9f7c
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-04-17 12:15:00 +0200

    Feat: integrate Peppol eligibility check in PendingBox component

--- src/modules/billing/invoice/components/PendingBox.tsx
+++ src/modules/billing/invoice/components/PendingBox.tsx
@@ -2,8 +2,12 @@ import { useParams } from 'next/navigation'
 
 import { Box, Button } from '@digico/ui'
 
+import { useReadInvoice } from '@billing/invoice/hooks/queries'
+
 import { ButtonSendToPeppol } from '@billing/invoice/components/ButtonSendToPeppol'
 import { useRouteTenant } from 'helpers/route-tenant'
+
+import { isPeppolEligible } from '../../../../data/peppol-eligible-country-codes'
 
 import { ButtonDownloadInvoice } from './ButtonDownloadInvoice'
 import { ButtonPrintInvoice } from './ButtonPrintInvoice'
@@ -11,6 +15,9 @@ import { ButtonPrintInvoice } from './ButtonPrintInvoice'
 export const PendingBox = () => {
     const { id } = useParams()
     const routeTenant = useRouteTenant()
+    const { data: invoice } = useReadInvoice(id)
+    const vatNumber = invoice?.recipient?.vat_number ?? ''
+    const isEligible = isPeppolEligible(vatNumber)
 
     return (
         <Box className="flex flex-col gap-4">
@@ -18,7 +25,7 @@ export const PendingBox = () => {
                 <ButtonPrintInvoice className="flex-1" />
                 <ButtonDownloadInvoice className="flex-1" />
             </div>
-            <ButtonSendToPeppol className="flex-1" />
+            {isEligible && <ButtonSendToPeppol className="flex-1" />}
             <Button href={routeTenant.get(`/billing/invoice/${id}/email`)} className="flex-1" intent={'main'}>
                 Envoyer par email
             </Button>
