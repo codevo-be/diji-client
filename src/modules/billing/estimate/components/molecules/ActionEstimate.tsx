23281a370bd92be1801c4b82a31cc3f10081de62
Maxime Fondu <maximefondu@icloud.com>
2025-04-21 10:03:01 +0200

    Add button create invoice from estimate

--- src/modules/billing/estimate/components/molecules/ActionEstimate.tsx
+++ src/modules/billing/estimate/components/molecules/ActionEstimate.tsx
@@ -1,12 +1,13 @@
 import { useParams } from 'next/navigation'
 
-import { ESTIMATE_STATUS_DRAFT } from '@billing/estimate/data/estimate-statuses'
+import { ESTIMATE_STATUS_ACCEPTED, ESTIMATE_STATUS_DRAFT, ESTIMATE_STATUS_EXPIRED } from '@billing/estimate/data/estimate-statuses'
 import { Grid } from '@digico/ui'
 
 import { useReadEstimate } from '@billing/estimate/hooks/queries'
 
 import { DraftBox } from './DraftBox'
 import { PendingBox } from './PendingBox'
+import { ValidateBox } from './ValidateBox'
 
 export const ActionEstimate = () => {
     const { id } = useParams()
@@ -20,6 +21,14 @@ export const ActionEstimate = () => {
         return (
             <Grid.Col>
                 <DraftBox />
+            </Grid.Col>
+        )
+    }
+
+    if (data.status === ESTIMATE_STATUS_ACCEPTED || data.status === ESTIMATE_STATUS_EXPIRED) {
+        return (
+            <Grid.Col>
+                <ValidateBox />
             </Grid.Col>
         )
     }
