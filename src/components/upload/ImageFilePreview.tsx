68f6b5da509c4d0e645624e6a367eb3b89bddbdd
Alexis Vanden Broeck <alexis.vandenbroeck@gmail.com>
2025-05-05 17:16:22 +0200

    Fix pour reprendre l'url

--- src/components/upload/ImageFilePreview.tsx
+++ src/components/upload/ImageFilePreview.tsx
@@ -1,23 +1,12 @@
-import { useEffect, useState } from 'react'
 import { ImageBuilder } from '@digico/ui'
 
-import getItemUploadBlob from '../../services/upload/get-item-upload-blob'
 import { UploadType } from '../../types/upload.types'
 
 export default function ImageFilePreview(file: UploadType) {
 
-    const [url, setUrl] = useState<string>('');
-
-    useEffect(() => {
-        const result = getItemUploadBlob(file.url);
-        result.then(r => {
-            setUrl(r)
-        });
-    }, [])
-
     return (
         <div>
-            <ImageBuilder src={url} className={''} />
+            <ImageBuilder src={file.url} className={''} />
         </div>
     )
 }
