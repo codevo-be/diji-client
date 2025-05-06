8a471208d8768f63197faee3bba73021a680e551
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-05-06 16:25:32 +0200

    Feat: refactor Header component to use tenant data fetching for logo rendering

--- src/modules/billing/document/Header.tsx
+++ src/modules/billing/document/Header.tsx
@@ -1,4 +1,4 @@
-import { useReadMeta } from 'hooks/queries/meta/useReadMeta'
+import { useReadTenant } from '../../../hooks/queries/tenant/useReadTenant'
 import { CreditNoteType } from '@billing/credit-note/types/credit-note'
 import { InvoiceType } from '@billing/invoice/types/invoice'
 import { RecurringInvoiceType } from '@billing/recurring-invoice/types/recurring-invoice'
@@ -10,11 +10,12 @@ type Props = {
 }
 
 export const Header = ({ data }: Props) => {
-    const queryMeta = useReadMeta('tenant_billing_details')
+    const queryTenant = useReadTenant()
+
     return (
         <>
             {/* @ts-ignore */}
-            {queryMeta.isSuccess ? <img className="h-32 max-w-[24rem] object-contain mb-12" src={String(queryMeta.data?.value?.logo)} alt="Logo" /> : null}
+            {queryTenant.isSuccess ? <img className="h-32 max-w-[24rem] object-contain mb-12" src={String(queryTenant.data?.data.settings?.logo)} alt="Logo" /> : null}
 
             <div className="flex">
                 {/* @ts-ignore */}
