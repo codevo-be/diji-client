6ad01fac0b226bfbc521cac5b01e0e4dc4820c5e
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-05-07 09:27:24 +0200

    Feat: remove duplicate import of ExpenseType in ExpenseTable component

--- src/modules/expense/components/ExpenseTable.tsx
+++ src/modules/expense/components/ExpenseTable.tsx
@@ -4,13 +4,13 @@ import { useRouter } from 'next/navigation'
 
 import { Table, Tag } from '@digico/ui'
 import { DateHelper, formatCurrency } from '@digico/utils'
+import { ExpenseType } from '@expense/type/expense'
 import clsx from 'clsx'
 
 import { ContactType } from '@contact/types/contact'
 import { EXPENSE_TYPES } from '@expense/data/expense_types'
 
 import { useAuth } from 'helpers/auth-context/useAuth'
-import { ExpenseType } from '@expense/type/expense'
 
 type Props = {
     items: ContactType[]
