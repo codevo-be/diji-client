c3b0f3aefea6d1a2ebba827a0f3c82d3ebe90a2e
Alexis Vanden Broeck <alexis.vandenbroeck@gmail.com>
2025-05-07 10:07:30 +0200

    Début fonctionnel d'ajout de contact dans la db depuis invoice

--- src/modules/billing/invoice/components/form/UpdateFormInvoice.tsx
+++ src/modules/billing/invoice/components/form/UpdateFormInvoice.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'next/navigation'
 
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { INVOICE_STATUS_DRAFT } from '@billing/invoice/data/invoice-statuses'
 import { Box, Button, Form, Grid } from '@digico/ui'
@@ -7,6 +8,7 @@ import { Tabs } from '@digico/ui'
 
 import { useUpdateInvoice } from '@billing/invoice/hooks/mutations'
 import { useReadInvoice } from '@billing/invoice/hooks/queries'
+import { useCreateContact } from '@contact/hooks/mutations'
 import { useReadContacts } from '@contact/hooks/queries'
 import { InvoiceType } from '@billing/invoice/types/invoice'
 import { ContactType } from '@contact/types/contact'
@@ -19,6 +21,9 @@ export const UpdateFormInvoice = () => {
     const { data } = useReadInvoice(Number(id))
     const updateInvoice = useUpdateInvoice()
     const { data: contacts } = useReadContacts()
+    const { mutate: createContact } = useCreateContact()
+
+    const [currentContact, setCurrentContact] = useState<ContactType | undefined>(undefined)
 
     const form = useForm<InvoiceType>({
         values: data
@@ -30,13 +35,7 @@ export const UpdateFormInvoice = () => {
         if (!contact) {
             return
         }
-
-        //@ts-ignore
-        form.setValue('recipient', contact.billing_address)
-        form.setValue('recipient.name', contact.display_name)
-        form.setValue('recipient.email', contact.email)
-        form.setValue('recipient.phone', contact.phone)
-        form.setValue('recipient.vat_number', contact.vat_number)
+        setCurrentContact(contact)
     }
 
     const onUpdateField = (e: React.ChangeEvent<HTMLInputElement>, key: string) => {
@@ -45,6 +44,54 @@ export const UpdateFormInvoice = () => {
             [key]: e.target.value
         })
     }
+
+    const onAddContact = () => {
+        const values = form.getValues('recipient') as any
+
+        const contact: Omit<ContactType, 'id' | 'display_name'> = {
+            company_name: values.name,
+            email: values.email,
+            phone: values.phone,
+            vat_number: values.vat_number,
+            billing_address: {
+                street: values.street,
+                street_number: values.street_number,
+                city: values.city,
+                zipcode: values.zipcode,
+                country: values.country
+            }
+        }
+
+        // @ts-ignore TODO
+        createContact(contact, {
+            onSuccess: (data) => {
+                const contactRes = data.data;
+
+                onSelectContact(contactRes.id);
+            }
+        });
+    }
+
+    useEffect(() => {
+        if (currentContact) {
+            form.resetField('recipient.name');
+            form.resetField('recipient.vat_number');
+            form.resetField('recipient.email');
+            form.resetField('recipient.phone');
+            form.resetField('recipient.street');
+            form.resetField('recipient.street_number');
+            form.resetField('recipient.city');
+            form.resetField('recipient.zipcode');
+            form.resetField('recipient.country');
+
+            // @ts-ignore
+            form.setValue('recipient', currentContact.billing_address)
+            form.setValue('recipient.name', currentContact.display_name)
+            form.setValue('recipient.email', currentContact.email)
+            form.setValue('recipient.phone', currentContact.phone)
+            form.setValue('recipient.vat_number', currentContact.vat_number)
+        }
+    }, [currentContact])
 
     if (data?.status !== INVOICE_STATUS_DRAFT) {
         return null
@@ -73,7 +120,7 @@ export const UpdateFormInvoice = () => {
                         </Form.Row>
                     </Form.Group>
                 </Box>
-                <Box>
+                <Box className={"transition-all duration-500"}>
                     <Tabs defaultStep={'recipient'}>
                         <Tabs.Head id="issuer">Expéditeur</Tabs.Head>
                         <Tabs.Head id="recipient">Destinataire</Tabs.Head>
@@ -98,6 +145,13 @@ export const UpdateFormInvoice = () => {
                             <RecipientFields />
                         </Tabs.Content>
                     </Tabs>
+
+                    {
+                        <Button type={"button"} className={"mt-12 w-full"} intent={"grey200"} onClick={onAddContact}>
+                            Créer fiche contact
+                        </Button>
+                    }
+
                     <Button className="w-full mt-12" isLoading={updateInvoice.isPending}>
                         Mettre à jour
                     </Button>
