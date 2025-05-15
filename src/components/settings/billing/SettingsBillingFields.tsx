c8461fa5dd31aa287687888cf913dac6e548535d
Thibaut Roderkerken Bogaert <thibaut3108@hotmail.com>
2025-05-15 10:44:33 +0200

    Feat: add billing number fields for invoices and credit notes in SettingsBillingFields component

--- src/components/settings/billing/SettingsBillingFields.tsx
+++ src/components/settings/billing/SettingsBillingFields.tsx
@@ -11,9 +11,10 @@ export const SettingsBillingFields = () => {
                     <Form.Field label="Adresse email" name="email" id="email" placeholder="info@diji.be" />
                     <Form.Field label="Numéro de téléphone" name="phone" id="phone" placeholder="0482/24.24.19" />
                     <Form.Field label="Numéro de tva" name="vat_number" id="vat_number" placeholder="0456.354.131" />
-                    <Form.Field label="IBAN" name="iban" id="iban" placeholder="45 6245 6225 1951" />
+                    <Form.Field label="IBAN" name="iban" id="iban" placeholder="BE45 6245 6225 1951" />
                 </Form.Row>
             </Form.Group>
+
             <Form.Group title="Adresse">
                 <Form.Row>
                     <Form.Field label="Rue" name="street" id="street" placeholder="Route de marche" />
@@ -21,6 +22,19 @@ export const SettingsBillingFields = () => {
                     <Form.Field label="Ville" name="city" id="city" placeholder="Marche-En-Famenne" />
                     <Form.Field label="Code postal" name="zipcode" id="zipcode" placeholder="5590" />
                     <Form.Select label="Pays" name="country" options={countries} />
+                </Form.Row>
+            </Form.Group>
+
+            <Form.Group title="Facturation">
+                <Form.Row>
+                    <Form.Field label="Numéro de départ des factures" name="invoice_start_number" id="invoice_start_number" type="number" placeholder="50" />
+                    <Form.Field
+                        label="Numéro de départ des notes de crédit"
+                        name="credit_note_start_number"
+                        id="credit_note_start_number"
+                        type="number"
+                        placeholder="50"
+                    />
                 </Form.Row>
             </Form.Group>
         </>
