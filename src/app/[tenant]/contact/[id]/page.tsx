439444f62c6b9208c47a494ebdc85174b7e3abb8
Maxime Fondu <maximefondu@icloud.com>
2025-07-02 16:39:27 +0200

    minor

--- src/app/[tenant]/contact/[id]/page.tsx
+++ src/app/[tenant]/contact/[id]/page.tsx
@@ -1,12 +1,27 @@
 'use client'
 
-import { Grid, PageHeader } from '@digico/ui'
+import { useParams } from 'next/navigation'
+
+import { Button, Grid, PageHeader } from '@digico/ui'
+
+import { useDestroyContact } from '@contact/hooks/mutations'
 
 import { UpdateContactForm } from '@contact/components/form/UpdateContactForm'
 import { useRouteTenant } from 'helpers/route-tenant'
 
 export default function Page() {
+    const { id } = useParams()
     const routerTenant = useRouteTenant()
+
+    const removeContact = useDestroyContact()
+
+    const onRemoveContact = () => {
+        removeContact.mutate(Number(id), {
+            onSuccess: () => {
+                routerTenant.push('/contact')
+            }
+        })
+    }
 
     return (
         <Grid>
@@ -14,6 +29,7 @@ export default function Page() {
                 <PageHeader label="Retour" href={routerTenant.get('/contact')}>
                     Contacts
                 </PageHeader>
+                <Button onClick={onRemoveContact}>Supprimer</Button>
             </Grid.Col>
             <Grid.Col>
                 <UpdateContactForm />
