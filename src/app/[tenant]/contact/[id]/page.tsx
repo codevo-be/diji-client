03675e14690b66c789ec8c351377e6089c07443a
Maxime Fondu <maximefondu@icloud.com>
2025-07-03 08:55:53 +0200

    fix back customer

--- src/app/[tenant]/contact/[id]/page.tsx
+++ src/app/[tenant]/contact/[id]/page.tsx
@@ -2,11 +2,12 @@
 
 import { useParams } from 'next/navigation'
 
-import { Button, Grid, PageHeader } from '@digico/ui'
+import { Button, Grid } from '@digico/ui'
 
 import { useDestroyContact } from '@contact/hooks/mutations'
 
 import { UpdateContactForm } from '@contact/components/form/UpdateContactForm'
+import { PageHeader } from '@helpers/PageHeader'
 import { useRouteTenant } from 'helpers/route-tenant'
 
 export default function Page() {
@@ -25,10 +26,8 @@ export default function Page() {
 
     return (
         <Grid>
-            <Grid.Col className="flex justify-between item-start">
-                <PageHeader label="Retour" href={routerTenant.get('/contact')}>
-                    Contacts
-                </PageHeader>
+            <Grid.Col className="flex justify-between items-start">
+                <PageHeader label="Retour">Contacts</PageHeader>
                 <Button intent={'error'} onClick={onRemoveContact}>
                     Supprimer
                 </Button>
